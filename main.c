ffbf31e2c2450ede8202044f7580c0300cccc5af
Mohamed Eldawy <imohamedeldawy@gmail.com>
2026-04-16 17:43:45 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,46 @@
-#include "APP.h"
+#include "STD_TYPES.h"
+
+#include "MOTOR_interface.h"
+#include "JOYSTICK_interface.h"
+#include "BATTERY_interface.h"
 
 int main(void)
 {
-    APP_Init();
+    u8 Local_u8Direction;
 
-    while (1)
+    MOTOR_Init();
+    JOYSTICK_Init();
+    BATTERY_Init();
+
+    while(1)
     {
-        APP_Run();
+        Local_u8Direction = JOYSTICK_GetDirection();
+
+        switch(Local_u8Direction)
+        {
+            case JOYSTICK_FORWARD:
+                MOTOR_Forward();
+            break;
+
+            case JOYSTICK_BACKWARD:
+                MOTOR_Backward();
+            break;
+
+            case JOYSTICK_LEFT:
+                MOTOR_Left();
+            break;
+
+            case JOYSTICK_RIGHT:
+                MOTOR_Right();
+            break;
+
+            default:
+                MOTOR_Stop();
+            break;
+        }
+
+        BATTERY_CheckStatus();
     }
+
+    return 0;
 }
