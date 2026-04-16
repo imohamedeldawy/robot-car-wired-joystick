6d4b884f28f8febe08ba31db85ab824d1dece22d
Mohamed Eldawy <imohamedeldawy@gmail.com>
2026-04-16 17:40:40 +0200

    Update JOYSTICK_interface.h

--- HAL/JOYSTICK/JOYSTICK_interface.h
+++ HAL/JOYSTICK/JOYSTICK_interface.h
@@ -1,17 +1,15 @@
-#ifndef JOYSTICK_INTERFACE_H_
-#define JOYSTICK_INTERFACE_H_
+#ifndef JOYSTICK_INTERFACE_H
+#define JOYSTICK_INTERFACE_H
 
 #include "STD_TYPES.h"
 
-/* Directions */
-#define JOY_FORWARD   1
-#define JOY_BACKWARD  2
-#define JOY_LEFT      3
-#define JOY_RIGHT     4
-#define JOY_STOP      5
+#define JOYSTICK_STOP      0
+#define JOYSTICK_FORWARD   1
+#define JOYSTICK_BACKWARD  2
+#define JOYSTICK_LEFT      3
+#define JOYSTICK_RIGHT     4
 
-/* Function Prototypes */
 void JOYSTICK_Init(void);
 u8 JOYSTICK_GetDirection(void);
 
-#endif /* JOYSTICK_INTERFACE_H_ */
+#endif
