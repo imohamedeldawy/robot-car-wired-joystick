6bc30664d47fd6537b4acda8f081389a9811a469
Mohamed Eldawy <imohamedeldawy@gmail.com>
2026-04-16 17:31:49 +0200

    Update ADC_interface.h

--- MCAL/ADC/ADC_interface.h
+++ MCAL/ADC/ADC_interface.h
@@ -1,19 +1,9 @@
 #ifndef ADC_INTERFACE_H
 #define ADC_INTERFACE_H
 
-#include <stdint.h>
-#include <stddef.h>
-
-#define ADC_TOP     1023.0
+#include "STD_TYPES.h"
 
 void ADC_Init(void);
-
-uint16_t ADC_Get(uint8_t channel);
-
-double ADC_GetScaled(uint8_t channel);
-
-void ADC_GetAll(uint16_t *channels);
-
-void ADC_GetAllScaled(double *channels);
+u16 ADC_ReadChannel(u8 Copy_u8Channel);
 
 #endif
