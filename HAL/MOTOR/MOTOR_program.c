671244879790bb2048c42fe645d35089a7135fbb
Mohamed Eldawy <imohamedeldawy@gmail.com>
2026-04-16 17:39:22 +0200

    Update MOTOR_program.c

--- HAL/MOTOR/MOTOR_program.c
+++ HAL/MOTOR/MOTOR_program.c
@@ -3,77 +3,61 @@
 #include "DIO_interface.h"
 
 #include "MOTOR_interface.h"
+#include "MOTOR_private.h"
 #include "MOTOR_config.h"
 
 void MOTOR_Init(void)
 {
-    /* Set all motor pins as output */
+    DIO_SetPinDirection(MOTOR_L_IN1_PORT, MOTOR_L_IN1_PIN, DIO_OUTPUT);
+    DIO_SetPinDirection(MOTOR_L_IN2_PORT, MOTOR_L_IN2_PIN, DIO_OUTPUT);
 
-    DIO_SetPinDirection(MOTOR_A_IN1_PORT, MOTOR_A_IN1_PIN, PIN_OUTPUT);
-    DIO_SetPinDirection(MOTOR_A_IN2_PORT, MOTOR_A_IN2_PIN, PIN_OUTPUT);
+    DIO_SetPinDirection(MOTOR_R_IN1_PORT, MOTOR_R_IN1_PIN, DIO_OUTPUT);
+    DIO_SetPinDirection(MOTOR_R_IN2_PORT, MOTOR_R_IN2_PIN, DIO_OUTPUT);
 
-    DIO_SetPinDirection(MOTOR_B_IN3_PORT, MOTOR_B_IN3_PIN, PIN_OUTPUT);
-    DIO_SetPinDirection(MOTOR_B_IN4_PORT, MOTOR_B_IN4_PIN, PIN_OUTPUT);
+    MOTOR_Stop();
 }
 
-/* Forward */
 void MOTOR_Forward(void)
 {
-    /* Motor A */
-    DIO_SetPinValue(MOTOR_A_IN1_PORT, MOTOR_A_IN1_PIN, PIN_HIGH);
-    DIO_SetPinValue(MOTOR_A_IN2_PORT, MOTOR_A_IN2_PIN, PIN_LOW);
+    DIO_SetPinValue(MOTOR_L_IN1_PORT, MOTOR_L_IN1_PIN, DIO_HIGH);
+    DIO_SetPinValue(MOTOR_L_IN2_PORT, MOTOR_L_IN2_PIN, DIO_LOW);
 
-    /* Motor B */
-    DIO_SetPinValue(MOTOR_B_IN3_PORT, MOTOR_B_IN3_PIN, PIN_HIGH);
-    DIO_SetPinValue(MOTOR_B_IN4_PORT, MOTOR_B_IN4_PIN, PIN_LOW);
+    DIO_SetPinValue(MOTOR_R_IN1_PORT, MOTOR_R_IN1_PIN, DIO_HIGH);
+    DIO_SetPinValue(MOTOR_R_IN2_PORT, MOTOR_R_IN2_PIN, DIO_LOW);
 }
 
-/* Backward */
 void MOTOR_Backward(void)
 {
-    /* Motor A */
-    DIO_SetPinValue(MOTOR_A_IN1_PORT, MOTOR_A_IN1_PIN, PIN_LOW);
-    DIO_SetPinValue(MOTOR_A_IN2_PORT, MOTOR_A_IN2_PIN, PIN_HIGH);
+    DIO_SetPinValue(MOTOR_L_IN1_PORT, MOTOR_L_IN1_PIN, DIO_LOW);
+    DIO_SetPinValue(MOTOR_L_IN2_PORT, MOTOR_L_IN2_PIN, DIO_HIGH);
 
-    /* Motor B */
-    DIO_SetPinValue(MOTOR_B_IN3_PORT, MOTOR_B_IN3_PIN, PIN_LOW);
-    DIO_SetPinValue(MOTOR_B_IN4_PORT, MOTOR_B_IN4_PIN, PIN_HIGH);
+    DIO_SetPinValue(MOTOR_R_IN1_PORT, MOTOR_R_IN1_PIN, DIO_LOW);
+    DIO_SetPinValue(MOTOR_R_IN2_PORT, MOTOR_R_IN2_PIN, DIO_HIGH);
 }
 
-/* Right */
-void MOTOR_Right(void)
-{
-    /* Left motor ON, right motor OFF */
-
-    /* Motor A */
-    DIO_SetPinValue(MOTOR_A_IN1_PORT, MOTOR_A_IN1_PIN, PIN_HIGH);
-    DIO_SetPinValue(MOTOR_A_IN2_PORT, MOTOR_A_IN2_PIN, PIN_LOW);
-
-    /* Motor B */
-    DIO_SetPinValue(MOTOR_B_IN3_PORT, MOTOR_B_IN3_PIN, PIN_LOW);
-    DIO_SetPinValue(MOTOR_B_IN4_PORT, MOTOR_B_IN4_PIN, PIN_LOW);
-}
-
-/* Left */
 void MOTOR_Left(void)
 {
-    /* Right motor ON, left motor OFF */
+    DIO_SetPinValue(MOTOR_L_IN1_PORT, MOTOR_L_IN1_PIN, DIO_LOW);
+    DIO_SetPinValue(MOTOR_L_IN2_PORT, MOTOR_L_IN2_PIN, DIO_LOW);
 
-    /* Motor A */
-    DIO_SetPinValue(MOTOR_A_IN1_PORT, MOTOR_A_IN1_PIN, PIN_LOW);
-    DIO_SetPinValue(MOTOR_A_IN2_PORT, MOTOR_A_IN2_PIN, PIN_LOW);
-
-    /* Motor B */
-    DIO_SetPinValue(MOTOR_B_IN3_PORT, MOTOR_B_IN3_PIN, PIN_HIGH);
-    DIO_SetPinValue(MOTOR_B_IN4_PORT, MOTOR_B_IN4_PIN, PIN_LOW);
+    DIO_SetPinValue(MOTOR_R_IN1_PORT, MOTOR_R_IN1_PIN, DIO_HIGH);
+    DIO_SetPinValue(MOTOR_R_IN2_PORT, MOTOR_R_IN2_PIN, DIO_LOW);
 }
 
-/* Stop */
+void MOTOR_Right(void)
+{
+    DIO_SetPinValue(MOTOR_L_IN1_PORT, MOTOR_L_IN1_PIN, DIO_HIGH);
+    DIO_SetPinValue(MOTOR_L_IN2_PORT, MOTOR_L_IN2_PIN, DIO_LOW);
+
+    DIO_SetPinValue(MOTOR_R_IN1_PORT, MOTOR_R_IN1_PIN, DIO_LOW);
+    DIO_SetPinValue(MOTOR_R_IN2_PORT, MOTOR_R_IN2_PIN, DIO_LOW);
+}
+
 void MOTOR_Stop(void)
 {
-    DIO_SetPinValue(MOTOR_A_IN1_PORT, MOTOR_A_IN1_PIN, PIN_LOW);
-    DIO_SetPinValue(MOTOR_A_IN2_PORT, MOTOR_A_IN2_PIN, PIN_LOW);
+    DIO_SetPinValue(MOTOR_L_IN1_PORT, MOTOR_L_IN1_PIN, DIO_LOW);
+    DIO_SetPinValue(MOTOR_L_IN2_PORT, MOTOR_L_IN2_PIN, DIO_LOW);
 
-    DIO_SetPinValue(MOTOR_B_IN3_PORT, MOTOR_B_IN3_PIN, PIN_LOW);
-    DIO_SetPinValue(MOTOR_B_IN4_PORT, MOTOR_B_IN4_PIN, PIN_LOW);
+    DIO_SetPinValue(MOTOR_R_IN1_PORT, MOTOR_R_IN1_PIN, DIO_LOW);
+    DIO_SetPinValue(MOTOR_R_IN2_PORT, MOTOR_R_IN2_PIN, DIO_LOW);
 }
