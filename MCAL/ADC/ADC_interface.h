053b44bfff8877e8f907a2fd78824063435e846e
Mohamed Eldawy <imohamedeldawy@gmail.com>
2026-04-16 17:03:45 +0200

    Update ADC_interface.h

--- MCAL/ADC/ADC_interface.h
+++ MCAL/ADC/ADC_interface.h
@@ -1,24 +1,19 @@
-/*
- * ADC_interface.h
- *
- *  Created on: Dec 21, 2022
- *      Author: aminz
- */
+#ifndef ADC_INTERFACE_H
+#define ADC_INTERFACE_H
 
-#ifndef ADC_INTERFACE_H_
-#define ADC_INTERFACE_H_
+#include <stdint.h>
+#include <stddef.h>
 
-#define Channel_A0 0
-#define Channel_A1 1
-#define Channel_A2 2
-#define Channel_A3 3
-#define Channel_A4 4
-#define Channel_A5 5
-#define Channel_A6 6
-#define Channel_A7 7
+#define ADC_TOP     1023.0
 
-void ADC_Init (void); // ADC Initialization And Enable
-u16 ADC_Read (u8 Channel); // Read From The ADC Channel
-u32 Mapping (u32 Range1_max,u32 Range1_min,u32 Range2_max,u32 Range2_min,u32 Range1_reading);
+void ADC_Init(void);
 
-#endif /* ADC_INTERFACE_H_ */
+uint16_t ADC_Get(uint8_t channel);
+
+double ADC_GetScaled(uint8_t channel);
+
+void ADC_GetAll(uint16_t *channels);
+
+void ADC_GetAllScaled(double *channels);
+
+#endif
